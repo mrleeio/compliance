b91719368c84dc57db98d917156810eee1829049
Michael Lee <michael@mrlee.io>
2023-08-01 15:03:59 -0600

    Print event payload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    // Write code here
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // eslint-disable-next-line no-console
+    console.log(`The event payload: ${payload}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
